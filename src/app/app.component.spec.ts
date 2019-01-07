5eadedf292bc52aeaaf5d3938b1bd31d4020a8f0
dieguito <diegoteka@hotmail.com>
2019-01-07 05:09:21 -0300

    examen

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,87 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mpower!');
-  }));
+import { FormsModule } from '@angular/forms';
+import { WeatherServiceService } from './services/weather-service.service';
+import { Observable, of } from 'rxjs';
+import { API_MENDOZA_HOY, API_BAIRES_HOY } from './mock-data-service';
+
+
+//Mock service 
+class MockWeatherServiceClass { 
+    getWeather(provincia: string, periodo: string): Observable<any>{  
+      return (provincia === 'Mendoza') ? of(API_MENDOZA_HOY) : of(API_BAIRES_HOY); 
+  }
+}
+
+
+describe('COMPONENT UNIT TEST',() =>{
+
+let fixture: ComponentFixture<AppComponent>;
+let comp:AppComponent;
+let userService:WeatherServiceService;
+
+beforeEach(() => {
+  TestBed.configureTestingModule({
+    imports: [ FormsModule ],
+    declarations: [
+      AppComponent
+    ],
+    providers: [
+     /* { provide: WeatherServiceService, useValue: MockWeatherServiceValue }*/
+      { provide: WeatherServiceService, useClass: MockWeatherServiceClass }
+    ]
+  })
+ 
+  fixture = TestBed.createComponent(AppComponent);
+  comp    = fixture.componentInstance;
+
+  // UserService from the root injector
+  userService = TestBed.get(WeatherServiceService);
+
+
 });
+
+
+it('Metodo consultar del componente que usa el servicio simulado', () => {
+  comp.provinciaselected = 'Buenos Aires';
+  comp.periodoselected = 'Hoy';
+  comp.consultar();
+  fixture.detectChanges();
+  expect(comp.temp).not.toBeNull();
+});
+
+it('Metodo consultar del componente que usa el servicio simulado', () => {
+  comp.provinciaselected = 'Mendoza';
+  comp.periodoselected = 'Hoy';
+  comp.consultar();
+  fixture.detectChanges();
+  expect(comp.temp).not.toBeNull();
+});
+
+
+it('title no sea indefinido', () => {
+  expect(comp.title).not.toBeUndefined();
+});
+
+it('title tenga el valor que corresponde', () => {
+  expect(comp.title).toEqual('Examen - Diego Morini');
+});
+
+it('Fecha actual no sea nula', () => {
+  comp.ngOnInit();
+  expect(comp.fstr).not.toBeNull();
+});
+
+
+
+})
+
+
+/*Testing with useValue in provider------------------------------------------
+let MockWeatherServiceValue: Partial<WeatherServiceService>;
+let fixture: ComponentFixture<AppComponent>;
+let comp,userService;
+
+MockWeatherServiceValue = {
+  ServiceActive : true
+}*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component'; 
 
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
